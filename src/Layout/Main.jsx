3252a046d853b2b753d77689820955ba258d9ed5
OMI786I <sohrawardy1998@gmail.com>
2023-10-26 23:25:35 +0600

    basic layout for footer and header

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer";
+import Navbar from "../Pages/Shared/Navbar";
 
 const Main = () => {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto">
+      <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
